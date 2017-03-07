33885008b090b0533e9d613b15cf07f081b6f927
Toni de la Fuente <toni.delafuente@alfresco.com>
2017-03-07 16:01:33 -0500

    fixed share javascripconsole

--- quickstart-for-custom/my_customization.sh
+++ quickstart-for-custom/my_customization.sh
@@ -38,7 +38,7 @@ chown tomcat:tomcat /etc/tomcat/context.xml
 curl -sL 'https://s3-eu-west-1.amazonaws.com/road2alfresco-utilities/jar-modules/platform/javascript-console-repo-0.7-SNAPSHOT.jar' -o /usr/share/tomcat-alfresco/modules/platform/javascript-console-repo-0.7-SNAPSHOT.jar
 curl -sL 'https://s3-eu-west-1.amazonaws.com/road2alfresco-utilities/jar-modules/platform/r2a-bulk-import-api-1.0-SNAPSHOT.jar' -o /usr/share/tomcat-alfresco/modules/platform/r2a-bulk-import-api-1.0-SNAPSHOT.jar
 curl -sL 'https://s3-eu-west-1.amazonaws.com/road2alfresco-utilities/jar-modules/platform/r2a-repo-jar-1.0-SNAPSHOT.jar' -o /usr/share/tomcat-alfresco/modules/platform/r2a-repo-jar-1.0-SNAPSHOT.jar
-curl -sL 'https://s3-eu-west-1.amazonaws.com/road2alfresco-utilities/jar-modules/share/javascript-console-share-0.7-SNAPSHOT.jar' -o /usr/share/tomcat-alfresco/modules/share/javascript-console-share-0.7-SNAPSHOT.jar
+curl -sL 'https://s3-eu-west-1.amazonaws.com/road2alfresco-utilities/jar-modules/share/javascript-console-share-0.7-SNAPSHOT.jar' -o /usr/share/tomcat-share/modules/share/javascript-console-share-0.7-SNAPSHOT.jar
 #curl -sL 'https://s3-eu-west-1.amazonaws.com/road2alfresco-utilities/p6spy-3.0.0.jar' -o /usr/share/tomcat-alfresco/lib/p6spy-3.0.0.jar
 #curl -sL 'https://s3-eu-west-1.amazonaws.com/road2alfresco-utilities/spy.properties' -o /usr/share/tomcat-alfresco/lib/spy.properties
 chown tomcat:tomcat /usr/share/tomcat-alfresco/lib/*
