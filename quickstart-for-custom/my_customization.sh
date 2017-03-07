84499b3c1691f531a632cbfacbbca52775137e19
Toni de la Fuente <toni.delafuente@alfresco.com>
2017-03-07 13:55:24 -0500

    removed dbspy

--- quickstart-for-custom/my_customization.sh
+++ quickstart-for-custom/my_customization.sh
@@ -39,8 +39,8 @@ curl -sL 'https://s3-eu-west-1.amazonaws.com/road2alfresco-utilities/jar-modules
 curl -sL 'https://s3-eu-west-1.amazonaws.com/road2alfresco-utilities/jar-modules/platform/r2a-bulk-import-api-1.0-SNAPSHOT.jar' -o /usr/share/tomcat-alfresco/modules/platform/r2a-bulk-import-api-1.0-SNAPSHOT.jar
 curl -sL 'https://s3-eu-west-1.amazonaws.com/road2alfresco-utilities/jar-modules/platform/r2a-repo-jar-1.0-SNAPSHOT.jar' -o /usr/share/tomcat-alfresco/modules/platform/r2a-repo-jar-1.0-SNAPSHOT.jar
 curl -sL 'https://s3-eu-west-1.amazonaws.com/road2alfresco-utilities/jar-modules/share/javascript-console-share-0.7-SNAPSHOT.jar' -o /usr/share/tomcat-alfresco/modules/share/javascript-console-share-0.7-SNAPSHOT.jar
-curl -sL 'https://s3-eu-west-1.amazonaws.com/road2alfresco-utilities/p6spy-3.0.0.jar' -o /usr/share/tomcat-alfresco/lib/p6spy-3.0.0.jar
-curl -sL 'https://s3-eu-west-1.amazonaws.com/road2alfresco-utilities/spy.properties' -o /usr/share/tomcat-alfresco/lib/spy.properties
+#curl -sL 'https://s3-eu-west-1.amazonaws.com/road2alfresco-utilities/p6spy-3.0.0.jar' -o /usr/share/tomcat-alfresco/lib/p6spy-3.0.0.jar
+#curl -sL 'https://s3-eu-west-1.amazonaws.com/road2alfresco-utilities/spy.properties' -o /usr/share/tomcat-alfresco/lib/spy.properties
 chown tomcat:tomcat /usr/share/tomcat-alfresco/lib/*
 chown tomcat:tomcat  /usr/share/tomcat-alfresco/modules/platform/*
 chown tomcat:tomcat  /usr/share/tomcat-alfresco/modules/share/*
